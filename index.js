b914a4a057a7364170f17c22b71bd30ec5552131
Kamon <harrellkamon@gmail.com>
2017-05-09 14:46:29 -0600

    graphing based on data

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 function makeCircle() {
 
-  circleRadii = [40, 20, 10]
+  circleRadii = [30, 70, 110]
 
  var svgContainer = d3.select("body").append("svg")
-  .attr("width", 600)
-  .attr("height", 100);
+  .attr("width", 200)
+  .attr("height", 200)
+  .style("border", "2px solid black")
 
  var circles = svgContainer.selectAll("circle")
   .data(circleRadii)
   .enter()
   .append("circle")
 
- var circleAttributes = circles
-  .attr("cx", 50)
-  .attr("cy", 50)
-  .attr("r", function (d) { return d; })
-  .style("fill", function(d) {
-     var returnColor
-     if (d === 40) { returnColor = "green"
-     } else if (d === 20) { returnColor = "purple"
-     } else if (d === 10) { returnColor = "red" }
-     return returnColor
-   })
+  var circleAtts = circles
+  .attr("cx", function (d) { return d })
+  .attr("cy", function (d) { return d })
+  .attr("r", 20)
 }
