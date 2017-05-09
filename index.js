90731cee4557981cc7ea035c4c0b57067f6b556e
Kamon <harrellkamon@gmail.com>
2017-05-09 13:41:33 -0600

    added d3 function that generates a dom element based on a dataset

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 function makeCircle() {
 
-  d3.select("body")
-    .append("svg")
-    .attr("width", 50)
-    .attr("height", 50)
-    .append("circle")
-    .attr("cx", 25)
-    .attr("cy", 25)
-    .attr("r", 25)
-    .style("fill", "red");
+    var theData = [ 1, 2, 3, 4]
+
+    var p = d3.select("body").selectAll("p")
+      .data(theData)
+      .enter()
+      .append("p")
+      .text("hello")
 
 }
